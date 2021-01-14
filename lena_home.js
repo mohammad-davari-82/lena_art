e11757789f937dc98eda901bb93f2211b52661ba
mohammad-davari-82 <mohammaddavarzani1998@gmail.com>
2021-01-14 10:51:10 +0330

    added header

--- lena_home.js
+++ lena_home.js
@@ -1,19 +1,27 @@
 var myVar;
-            
-            function myFunction() {
-              myVar = setTimeout(showPage, 3000);
-            }
-            
-            function showPage() {
-              document.getElementById("loader").style.display = "none";
-              document.getElementById("loading").style.display = "block";
-            }
-            var header = document.getElementById("icon-bar");
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loading").style.display = "block";
+}
+var header = document.getElementById("icon-bar");
 var btns = header.getElementsByClassName("bt");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
